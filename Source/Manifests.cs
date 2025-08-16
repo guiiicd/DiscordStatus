4ce86382498e36b673977c292f66529c39eee1fd
Guilherme Carnietto Depizol <24928611+guiiicd@users.noreply.github.com>
2025-08-16 11:05:19 -0400

    ATTs

--- Source/Manifests.cs
+++ Source/Manifests.cs
@@ -5,8 +5,8 @@
     public sealed partial class DiscordStatus : BasePlugin
     {
         public override string ModuleName => "DiscordStatus";
-        public override string ModuleVersion => "v3.2";
-        public override string ModuleAuthor => "Tian";
+        public override string ModuleVersion => "v3.4";
+        public override string ModuleAuthor => "Guilheme";
         public override string ModuleDescription => "Showing Server Status on Discord";
     }
 }
